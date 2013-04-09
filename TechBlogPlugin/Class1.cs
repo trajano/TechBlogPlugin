366a7e3bef88208692051d396f5d30955c270da6
Archimedes Trajano <arch@trajano.net>
2013-04-09 09:06:15 -0400

    A no-op plugin

--- TechBlogPlugin/Class1.cs
+++ TechBlogPlugin/Class1.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Forms;
+using WindowsLive.Writer.Api;
 
 namespace TechBlogPlugin
 {
-    public class Class1
+    [WriterPluginAttribute("11293a99-23ba-487a-97da-4696e47ac4b8",
+    "Simple Insert Code",
+    PublisherUrl = "http://www.trajano.net",
+    Description = "Insert code or pre tags to a blog post selection")]
+    [InsertableContentSourceAttribute("Insert Code", SidebarText = "Insert code tags to a blog post selection.")]
+    public class Class1 : ContentSource
     {
+        public override DialogResult CreateContent(IWin32Window dialogOwner, ref string content)
+        {
+            return DialogResult.OK;
+        }
     }
 }
