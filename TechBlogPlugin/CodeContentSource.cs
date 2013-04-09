afb339203c6141c2ef8d0071b9fc4807038ac478
Archimedes Trajano <arch@trajano.net>
2013-04-09 09:54:15 -0400

    Implement insertion code

--- TechBlogPlugin/CodeContentSource.cs
+++ TechBlogPlugin/CodeContentSource.cs
@@ -11,10 +11,18 @@ namespace TechBlogPlugin
     PublisherUrl = "http://www.trajano.net",
     Description = "Insert code or pre tags to a blog post selection")]
     [InsertableContentSourceAttribute("Insert Code", SidebarText = "Insert code tags to a blog post selection.")]
-    public class Class1 : ContentSource
+    public class CodeContentSource : ContentSource
     {
         public override DialogResult CreateContent(IWin32Window dialogOwner, ref string content)
         {
+            if (content.Length == 0)
+            {
+                content = "<code>code</code>";
+            }
+            else
+            {
+                content = "<code>" + content + "</code>";
+            }
             return DialogResult.OK;
         }
     }
